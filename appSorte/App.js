bb0d8fea614bc0b245a44808e4aac0bafda33784
John <jhow.mito11@gmail.com>
2021-12-09 14:37:43 -0300

    commit primeiro

---
+++ appSorte/App.js
@@ -0,0 +1,17 @@
+import React,{ Component} from "react";
+import { View, Text } from "react-native";
+import css from './styles'
+
+
+export default class appSorte extends Component {
+ render(){
+  return (
+    <View
+    style ={css.container}
+    >
+
+<Text> Ola </Text>
+    </View>
+   );
+ }
+}
